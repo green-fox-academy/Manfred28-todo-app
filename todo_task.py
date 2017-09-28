b3b3838a55538609395a1ba288d7e9602e8beaf8
Kozma István <pisti_kozma@hotmail.com>
2017-09-28 11:48:14 +0200

    feat(model): add class for todo items

---
+++ todo_task.py
@@ -0,0 +1,7 @@
+class TodoTask(object):
+    def __init__(self, complete, description):
+        self.complete = complete
+        self.description = description
+
+    def __str__(self):
+        return ("[x] " if self.complete else "[] ") + self.description
