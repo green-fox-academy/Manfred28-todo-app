98ca6622e8473e0adc3810990d2bf716ec8d083d
Kozma István <pisti_kozma@hotmail.com>
2017-09-28 12:32:47 +0200

    feat(model): add TodoList class

---
+++ todo_task_list.py
@@ -0,0 +1,12 @@
+from todo_task import TodoTask
+
+class TodoList(object):
+    def __init__(self):
+        self.task_list = []
+        self.read_tasks_from_file()
+
+    def read_tasks_from_file(self):
+        with open("todo_data", "r") as tasks:
+            for task in tasks:
+                self.task_list.append(TodoTask(bool(task[0]), task[1:]))
+    
