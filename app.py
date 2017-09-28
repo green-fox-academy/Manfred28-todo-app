c9b8f5308862f95c77465b263d06b66400d18684
Kozma István <pisti_kozma@hotmail.com>
2017-09-28 10:37:13 +0200

    feat(print_usage): store commands and add method to print them

---
+++ app.py
@@ -0,0 +1,20 @@
+from sys import argv
+
+class App():
+    def __init__(self):
+        self.commands = [
+            {"arg": "-l", "desc": "Lists all the tasks"},
+            {"arg": "-a", "desc": "Adds a new task"},
+            {"arg": "-r", "desc": "Removes an task"},
+            {"arg": "-c", "desc": "Completes an task"}
+        ]
+
+    def print_usage(self):
+        print("Command Line Todo application\n" +
+              "=============================" +
+              "\n" +
+              "Command line arguments:" +
+              self.commands[0]["arg"] + "   " + self.commands[0]["desc"] + "\n" +
+              self.commands[1]["arg"] + "   " + self.commands[1]["desc"] + "\n" +
+              self.commands[2]["arg"] + "   " + self.commands[3]["desc"] + "\n" +
+              self.commands[4]["arg"] + "   " + self.commands[4]["desc"] + "\n")
